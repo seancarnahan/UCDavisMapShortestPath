5572ad6c49d842abf5cacdd7c021096fc7a3116a
seancarnahan <Vhellendoorn@ucdavis.edu>
2019-06-04 18:12:38 -0700

    git init

---
+++ MapRouter.cpp
@@ -0,0 +1,88 @@
+#include "MapRouter.h"
+#include <cmath>
+
+const CMapRouter::TNodeID CMapRouter::InvalidNodeID = -1;
+
+CMapRouter::CMapRouter(){
+
+}
+
+CMapRouter::~CMapRouter(){
+
+}
+
+double CMapRouter::HaversineDistance(double lat1, double lon1, double lat2, double lon2){
+    auto DegreesToRadians = [](double deg){return M_PI * (deg) / 180.0;};
+	double LatRad1 = DegreesToRadians(lat1);
+	double LatRad2 = DegreesToRadians(lat2);
+	double LonRad1 = DegreesToRadians(lon1);
+	double LonRad2 = DegreesToRadians(lon2);
+	double DeltaLat = LatRad2 - LatRad1;
+	double DeltaLon = LonRad2 - LonRad1;
+	double DeltaLatSin = sin(DeltaLat/2);
+	double DeltaLonSin = sin(DeltaLon/2);	
+	double Computation = asin(sqrt(DeltaLatSin * DeltaLatSin + cos(LatRad1) * cos(LatRad2) * DeltaLonSin * DeltaLonSin));
+	const double EarthRadiusMiles = 3959.88;
+	
+	return 2 * EarthRadiusMiles * Computation;
+}        
+
+double CMapRouter::CalculateBearing(double lat1, double lon1,double lat2, double lon2){
+    auto DegreesToRadians = [](double deg){return M_PI * (deg) / 180.0;};
+    auto RadiansToDegrees = [](double rad){return 180.0 * (rad) / M_PI;};
+    double LatRad1 = DegreesToRadians(lat1);
+	double LatRad2 = DegreesToRadians(lat2);
+	double LonRad1 = DegreesToRadians(lon1);
+	double LonRad2 = DegreesToRadians(lon2);
+    double X = cos(LatRad2)*sin(LonRad2-LonRad1);
+    double Y = cos(LatRad1)*sin(LatRad2)-sin(LatRad1)*cos(LatRad2)*cos(LonRad2-LonRad1);
+    return RadiansToDegrees(atan2(X,Y));
+}
+
+bool CMapRouter::LoadMapAndRoutes(std::istream &osm, std::istream &stops, std::istream &routes){
+    // Your code HERE
+}
+
+size_t CMapRouter::NodeCount() const{
+    // Your code HERE
+}
+
+CMapRouter::TNodeID CMapRouter::GetSortedNodeIDByIndex(size_t index) const{
+    // Your code HERE
+}
+
+CMapRouter::TLocation CMapRouter::GetSortedNodeLocationByIndex(size_t index) const{
+    // Your code HERE
+}
+
+CMapRouter::TLocation CMapRouter::GetNodeLocationByID(TNodeID nodeid) const{
+    // Your code HERE
+}
+
+CMapRouter::TNodeID CMapRouter::GetNodeIDByStopID(TStopID stopid) const{
+    // Your code HERE
+}
+
+size_t CMapRouter::RouteCount() const{
+    // Your code HERE
+}
+
+std::string CMapRouter::GetSortedRouteNameByIndex(size_t index) const{
+    // Your code HERE
+}
+
+bool CMapRouter::GetRouteStopsByRouteName(const std::string &route, std::vector< TStopID > &stops){
+    // Your code HERE
+}
+
+double CMapRouter::FindShortestPath(TNodeID src, TNodeID dest, std::vector< TNodeID > &path){
+    // Your code HERE
+}
+
+double CMapRouter::FindFastestPath(TNodeID src, TNodeID dest, std::vector< TPathStep > &path){
+    // Your code HERE
+}
+
+bool CMapRouter::GetPathDescription(const std::vector< TPathStep > &path, std::vector< std::string > &desc) const{
+    // Your code HERE
+}
---
+++ MapRouter.h
@@ -0,0 +1,39 @@
+#ifndef MAPROUTER_H
+#define MAPROUTER_H
+
+#include <vector>
+#include <istream>
+
+class CMapRouter{
+    public:
+        using TNodeID = unsigned long;
+        using TStopID = unsigned long;
+        using TLocation = std::pair<double, double>;
+        using TPathStep = std::pair<std::string, TNodeID>;
+        
+        static const TNodeID InvalidNodeID;
+    private:
+        
+    public:
+        CMapRouter();
+        ~CMapRouter();
+        
+        static double HaversineDistance(double lat1, double lon1, double lat2, double lon2);
+        static double CalculateBearing(double lat1, double lon1,double lat2, double lon2);
+        
+        bool LoadMapAndRoutes(std::istream &osm, std::istream &stops, std::istream &routes);
+        size_t NodeCount() const;
+        TNodeID GetSortedNodeIDByIndex(size_t index) const;
+        TLocation GetSortedNodeLocationByIndex(size_t index) const;
+        TLocation GetNodeLocationByID(TNodeID nodeid) const;
+        TNodeID GetNodeIDByStopID(TStopID stopid) const;
+        size_t RouteCount() const;
+        std::string GetSortedRouteNameByIndex(size_t index) const;
+        bool GetRouteStopsByRouteName(const std::string &route, std::vector< TStopID > &stops);
+        
+        double FindShortestPath(TNodeID src, TNodeID dest, std::vector< TNodeID > &path);
+        double FindFastestPath(TNodeID src, TNodeID dest, std::vector< TPathStep > &path);
+        bool GetPathDescription(const std::vector< TPathStep > &path, std::vector< std::string > &desc) const;
+};
+
+#endif
